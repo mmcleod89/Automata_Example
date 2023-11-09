0fbc97172a25827dfb4c24035daf73d10d73b19a
Michael McLeod <michaelmcleod@ucl.ac.uk>
2023-11-09 11:30:59 +0000

    Initial commit

---
+++ include/automaton.h
@@ -0,0 +1,26 @@
+#ifndef AUTOMATON_H
+#define AUTOMATON_H
+
+#include <map>
+#include <vector>
+#include <algorithm>
+
+using std::vector;
+using std::map;
+using std::string;
+
+class Automaton
+{
+    public: 
+    Automaton(map<char, int> A, vector<vector<int>> M, vector<int> S);
+
+    bool Read(string word);
+
+    private:
+    int state = 0;
+    map<char, int> alphabet;
+    vector<vector<int>> transition_matrix;
+    vector<int> accepting_states;
+};
+
+#endif
